056e4a59f0e91542cd202b5cc672d9ef69e4d38d
Samia Mullen <syedamullen@live.com>
2020-06-15 23:09:51 -0400

    The Fizzlebuzz is finally done and working

--- src/main/java/com/ben/java/fizzbuzzing/App.java
+++ src/main/java/com/ben/java/fizzbuzzing/App.java
@@ -4,10 +4,21 @@ package com.ben.java.fizzbuzzing;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 & i % 3 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
     }
 }
